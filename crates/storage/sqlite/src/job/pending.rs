65c60ace85cdfa8ef389f0ddedfc6597da151fd2
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2025-03-27 03:39:58 +0000

    add function implemented

--- crates/storage/sqlite/src/job/pending.rs
+++ crates/storage/sqlite/src/job/pending.rs
@@ -48,24 +48,47 @@ impl PendingJobRepo for SqlitePendingJobRepo {
             scheduled_at: i64,
         }
 
-        let result: Option<RGet> = sqlx::query_as("SELECT scheduled_at FROM $1 WHERE job_id = $2")
-            .bind(&self.settings.pending_job_repo_table_name)
-            .bind(job_id.value().to_string())
-            .fetch_optional(&self.pool)
-            .await
-            .map_err(Self::map_sqlx_error)?;
+        let result: Option<RGet> = sqlx::query_as(
+            format!(
+                "SELECT scheduled_at FROM {} WHERE job_id = '$1'",
+                &self.settings.pending_job_repo_table_name
+            )
+            .as_str(),
+        )
+        .bind(job_id.value().to_string())
+        .fetch_optional(&self.pool)
+        .await
+        .map_err(Self::map_sqlx_error)?;
 
         match result {
             Some(result) => Ok(Some(PendingJob::new(
                 *job_id,
-                DateTime::from_timestamp_nanos(result.scheduled_at),
+                DateTime::from_timestamp_millis(result.scheduled_at).unwrap(),
             ))),
             None => Ok(None),
         }
     }
 
     async fn add(&self, job: PendingJob) -> storage::error::Result<()> {
-        todo!()
+        let existing_job = self.get(job.job_id()).await?;
+        if existing_job.is_some() {
+            return Err(storage::error::Error::AlreadyExists);
+        }
+
+        sqlx::query(
+            format!(
+                "INSERT INTO {} (job_id, scheduled_at) VALUES ('$1', $2)",
+                &self.settings.pending_job_repo_table_name,
+            )
+            .as_str(),
+        )
+        .bind(job.job_id().value().to_string())
+        .bind(job.scheduled_at().timestamp_millis())
+        .execute(&self.pool)
+        .await
+        .map_err(Self::map_sqlx_error)?;
+
+        Ok(())
     }
 
     async fn delete(&self, job_id: &JobId) -> storage::error::Result<PendingJob> {
@@ -87,5 +110,23 @@ mod tests {
         let settings = SqliteStorageSettings::default();
 
         SqlitePendingJobRepo::init(&pool, &settings).await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_add_get() {
+        let pool = SqlitePool::connect(":memory:").await.unwrap();
+        let settings = SqliteStorageSettings::default();
+        let repo = SqlitePendingJobRepo::new(pool, settings).await.unwrap();
+
+        let job = PendingJob::new(
+            JobId::new(),
+            DateTime::from_timestamp_millis(10000).unwrap(),
+        );
+        repo.add(job.clone()).await.unwrap();
+
+        let retrieved = repo.get(job.job_id()).await.unwrap().unwrap();
+
+        assert_eq!(retrieved.job_id(), job.job_id());
+        assert_eq!(retrieved.scheduled_at(), job.scheduled_at());
     }
 }
